2c623a8c79db701c6e5bb38d72e23b1145e4769f
Mdeaquin <mdeaquin21@gmail.com>
2025-08-13 23:58:21 -0500

    feat: draw palyer

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     game_on = 1
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
